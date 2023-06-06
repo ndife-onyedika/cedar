3b51d4cf96eef671532cae73103779e70d1d7962
Onyedika Ndife <onyedikandife@gmail.com>
2023-06-06 20:16:01 +0100

    Added signal for celery task failure

--- cedar/settings.py
+++ cedar/settings.py
@@ -56,12 +56,12 @@ LOGOUT_URL = "/accounts/logout/"
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.postgresql_psycopg2",
         "NAME": config("DATABASE_NAME"),
         "USER": config("DATABASE_USER"),
         "PASSWORD": config("DATABASE_PASSWORD"),
         "HOST": config("DATABASE_HOST"),
         "PORT": config("DATABASE_PORT"),
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
     }
 }
 
--- dashboard/models.py
+++ dashboard/models.py
@@ -1,3 +1,30 @@
-from django.db import models
+import socket
+from celery.signals import task_failure
+from django.core.mail import mail_admins
 
-# Create your models here.
+
+@task_failure.connect()
+def celery_task_failure_email(**kwargs):
+    """celery 4.0 onward has no method to send emails on failed tasks
+    so this event handler is intended to replace it
+    """
+
+    subject = "[Django][{queue_name}@{host}] Error: Task {sender.name} ({task_id}): {exception}".format(
+        queue_name="celery",  # `sender.queue` doesn't exist in 4.1?
+        host=socket.gethostname(),
+        **kwargs
+    )
+
+    message = """Task {sender.name} with id {task_id} raised exception:
+        {exception!r}
+
+        Task was called with args: {args} kwargs: {kwargs}.
+
+        The contents of the full traceback was:
+
+            {einfo}
+    """.format(
+        **kwargs
+    )
+
+    mail_admins(subject, message)
