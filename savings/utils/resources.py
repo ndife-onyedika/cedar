f89b3d48b034e721c0d20adc83582e2322ec0ea1
Onyedika Ndife <onyedikandife@gmail.com>
2025-01-29 12:12:42 +0100

    updates:

--- savings/utils/resources.py
+++ savings/utils/resources.py
@@ -4,11 +4,8 @@ from savings.models import SavingsCredit, SavingsDebit
 
 
 class BaseSavingsResource(resources.ModelResource):
-    def dehydrate_member_name(self, instance):
-        return instance.member.name.upper()
-
     class Meta:
-        fields = ("id", "member", "member_name", "amount", "reason", "created_at")
+        fields = ("id", "member", "amount", "reason", "created_at")
         export_order = fields
 
 
--- utils/admin_mixins.py
+++ utils/admin_mixins.py
@@ -1,49 +1,69 @@
+import logging
+
+from django.conf import settings
 from django.contrib import messages
-from django.http import HttpResponse, HttpResponseRedirect
 from django.contrib.auth.mixins import AccessMixin
-from accounts.models import Member
+from django.http import HttpResponse, HttpResponseRedirect
+
+logger = logging.getLogger(__name__)
 
 
 class InternalModelAdminMixin:
     """Mixin to catch all errors in the Django Admin and map them to user-visible errors."""
 
     def add_view(self, request, form_url="", extra_context=None):
+        if settings.DEBUG:
+            return super().add_view(request, form_url, extra_context)
+
         try:
             return super().add_view(request, form_url, extra_context)
         except Exception as e:
+            message = f"Error: {e}"
             self.message_user(
                 request=request,
-                message=f"Error: {e}",
+                message=message,
                 level=messages.ERROR,
             )
+            logger.error(message, exc_info=1)
+
             # This logic was cribbed from the `add_view()` handling here:
             # django/contrib/admin/options.py:response_post_save_add()
             # There might be a simpler way to do this, but it seems to do the job.
             return HttpResponseRedirect(request.path)
 
     def change_view(self, request, object_id, form_url="", extra_context=None):
+        if settings.DEBUG:
+            return super().change_view(request, object_id, form_url, extra_context)
+
         try:
             return super().change_view(request, object_id, form_url, extra_context)
         except Exception as e:
+            message = f"Error: {e}"
             self.message_user(
                 request=request,
-                message=f"Error: {e}",
+                message=message,
                 level=messages.ERROR,
             )
+            logger.error(message, exc_info=1)
             # This logic was cribbed from the `change_view()` handling here:
             # django/contrib/admin/options.py:response_post_save_add()
             # There might be a simpler way to do this, but it seems to do the job.
             return HttpResponseRedirect(request.path)
 
     def changelist_view(self, request, extra_context=None):
+        if settings.DEBUG:
+            return super().changelist_view(request, extra_context)
+
         try:
             return super().changelist_view(request, extra_context)
         except Exception as e:
+            message = f"Error: {e}"
             self.message_user(
                 request=request,
-                message=f"Error: {e}",
+                message=message,
                 level=messages.ERROR,
             )
+            logger.error(message, exc_info=1)
             # This logic was cribbed from the `changelist_view()` handling here:
             # django/contrib/admin/options.py:response_post_save_add()
             # There might be a simpler way to do this, but it seems to do the job.
